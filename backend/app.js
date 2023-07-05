772f3d8c7bb40933fd2658f5caa8f8edf7370608
Kirill <Kirill.pros@gmail.com>
2023-07-05 16:47:30 +0400

    Добавить защиту приложения

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,6 @@
 require('dotenv').config();
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
@@ -10,7 +12,15 @@ const { validateCreateUser, validateLogin } = require('./middlewares/validation'
 const { handleErrors } = require('./middlewares/handleErrors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
+
+// настройка лимитера запросов
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
 
 const corsOptions = {
   origin: '*',
@@ -20,19 +30,14 @@ const corsOptions = {
 
 const app = express();
 
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
+mongoose.connect(DB_URL);
 
 app.use(cors(corsOptions));
 app.use(express.json());
+app.use(helmet()); // настройка заголовков HTTP для защиты приложения
+app.use(limiter);
 
 app.use(requestLogger); // подключаем логгер запросов
-
-// краш-тест сервера
-app.get('/crash-test', () => {
-  setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
-  }, 0);
-});
 
 app.post('/signin', validateLogin, login);
 app.post('/signup', validateCreateUser, createUser);
